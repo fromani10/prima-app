a3ea25f842519a9871febb6a096d24e338f03a06
fromani10 <romanifede10@gmail.com>
2023-10-12 13:05:34 +0200

    inserito modelli e auto

--- Program.cs
+++ Program.cs
@@ -1,6 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Auto miaAuto;
+miaAuto = new Auto();
+miaAuto.stampa();
+
 class Auto{
+    //tipo
+    public enum motore
+    {
+        benzina,
+        disel,
+        elettrico
+    }
+    //proprietà
+    public string marca;
+    public string modello;
+    public motore tipoMotore;
+    public string colore;
+
+    //costruttore
+    public Auto()
+    {
+        this.marca = "Fiat";
+        this.modello = "punto";
+        this.tipoMotore = motore.disel;
+        this.colore = "verde";
+    }
+    public void stampa()
+    {
+        //stampo le caratteristiche dell auto
+        Console.WriteLine("marca: "+ this.marca);
+        Console.WriteLine("modello: "+ this.modello);
+        Console.WriteLine("motore: "+ this.tipoMotore);
+        Console.WriteLine("colore: "+ this.colore);
+
+    }
+
 
 
 
