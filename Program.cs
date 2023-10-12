9d7c1165187dca1fb3df33075f1ffb9483901b3b
fromani10 <romanifede10@gmail.com>
2023-10-12 17:07:28 +0200

    m

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Auto miaAuto;
-miaAuto = new Auto();
+
+miaAuto = new Auto("fiat", "multipla", Auto.motore.benzina, "grigio");
 miaAuto.stampaDescrizione();
 
-class Auto{
+class Auto
+{
     //tipo
     public enum motore
     {
@@ -16,6 +18,10 @@ class Auto{
     public string modello;
     public motore tipoMotore;
     public string colore;
+    public int carburante;
+    public bool accesa;
+    
+
 
     //costruttore
     public Auto()
@@ -28,7 +34,7 @@ class Auto{
     public void stampaDescrizione()
     {
         //stampo le caratteristiche dell auto
-       Console.WriteLine(descrizione());
+        Console.WriteLine(descrizione());
 
 
 
@@ -38,14 +44,47 @@ class Auto{
     {
         string result;
 
-        result = $"marca: {this.marca}"+"\n";//modo diverso per scrivere
-        result +=" modello: " + this.modello+"\n";
+        result = $"marca: {this.marca}" + "\n";//modo diverso per scrivere
+        result += " modello: " + this.modello + "\n";
         result += " motore: " + this.tipoMotore + "\n";
-        result+=" colore: " + this.colore;
+        result += " colore: " + this.colore+"\n";
+        
+       
         return result;
 
     }
+    public Auto(string marca, string modello, motore tipoMotore, string colore)
+    {
+        this.marca = marca;
+        this.modello = modello;
+        this.tipoMotore = tipoMotore;
+        this.colore = colore;
+        //this.accesa = accesa;
+        //this.carburante = carburante;
+    }
+   public void accendi()
+    {
+        //verifico il livello del carburante 
+        if (this.carburante > 0)
+        {
+            //se maggiore di zero si accende
+            this.accesa= true;
+        }
+        else
+        {
+            //se minore o uguale di zero spenta
+            this.accesa = false;
+        }
+
+    }
+    public void spegni()
+    {
+        this.accesa = false;
+    }
+
+
 }
+
 
 
 
